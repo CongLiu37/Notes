48910fbd691da053728e641279de8d7228c00e80
CongLiu37 <cong.liu20@imperial.ac.uk>
2021-09-29 00:23:54 +0100

    Notes

--- Update.sh
+++ Update.sh
@@ -1,4 +1,4 @@
 git pull
 git add --all
 git commit -m Notes
-git push https://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@github.com/CongLiu37/Notes.git
+git push https://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@github.com/CongLiu37/Notes.git
